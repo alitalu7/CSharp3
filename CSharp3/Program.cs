14142e7f94c531c9dbbbb1b32e34657a0caff0c5
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 21:12:42 +0300

    Add project files.

---
+++ CSharp3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int val1 = Convert.ToInt32(Console.ReadLine());
+int val2 = Convert.ToInt32(Console.ReadLine());
+int sum = solveMeFirst(val1, val2);
+Console.WriteLine(sum);
+
+static int solveMeFirst(int a, int b)
+{
+    return a + b;
+}
